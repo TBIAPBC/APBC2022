1acb46ad190f69a0547ff33c8583815a8becbe25
Lisa Korner <lisa_korner@hotmail.de>
2022-03-03 14:47:20 +0100

    added trimming of input string

--- A0/renrok-HelloWorld.py
+++ A0/renrok-HelloWorld.py
@@ -1,4 +1,3 @@
 with open('HelloWorld-test1.in', 'r') as reader:
     with open('HelloWorld-test1.out', 'w') as writer:
-        writer.write("Hello World!\n" + reader.read())
-        
+        writer.write("Hello World!\n" + reader.read().strip())
