ee203f9ce9395c1158c1695ef0484d1db687b564
Lisa Korner <lisa_korner@hotmail.de>
2022-03-03 15:45:15 +0100

    refactored output and added command line input file

--- A0/renrok-HelloWorld.py
+++ A0/renrok-HelloWorld.py
@@ -1,3 +1,13 @@
-with open('HelloWorld-test1.in', 'r') as reader:
-    with open('HelloWorld-test1.out', 'w') as writer:
-        writer.write("Hello World!\n" + reader.read().strip())
+import sys
+#checking if a input filename is given over the command line
+if (len(sys.argv) > 1):
+    inputFile = sys.argv[1]
+else:
+    inputFile = "HelloWorld-test1.in"
+#reading the input file and trim the text
+with open(inputFile, 'r') as reader:
+    inputText = reader.read().strip()
+#prints to standard output
+print("Hello World!\n" + inputText)
+    
+  
