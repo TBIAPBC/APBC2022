841106a77be36345b7d5254359f4cfef5600c611
DenSki92 <denisskibinski@live.de>
2022-03-04 09:25:59 +0100

    inserted Output via print

--- A0/DenSki92-HelloWorld.py
+++ A0/DenSki92-HelloWorld.py
@@ -3,7 +3,10 @@ import sys
 with open(sys.argv[1], "r") as r_file:
     text = r_file.read()
 
-with open("HelloWorld-test1.out", "w") as w_file:
-    w_file.write("Hello World\n")
-    w_file.write(text)
+print("Hello World\n" + text)
+
+
+#with open("HelloWorld-test1.out", "w") as w_file:
+#    w_file.write("Hello World\n")
+#    w_file.write(text)
 
