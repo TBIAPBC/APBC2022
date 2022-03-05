510db80622f0c00dd7ab3589842ac517578f77ec
Lisa Korner <lisa_korner@hotmail.de>
2022-03-05 15:17:15 +0100

    refactored structure and added comments

--- A1/renrok-WordCount.py
+++ A1/renrok-WordCount.py
@@ -1,55 +1,56 @@
-#-*- coding: utf-8 -*-
 import sys
 import re
-doIgnore = False
-doList = False 
-argumentInput = len(sys.argv)
-match argumentInput: 
-    case 0: 
-        print("Error")
-    case 1: 
-        print("No input file is given. Please enter a file name and retry")
-    case 2: 
-        inputFile = sys.argv[1]
-    case 3: 
-        inputFile = sys.argv[2] 
-        if "I" in sys.argv[1]:
-            doIgnore = True
-        elif "l" in sys.argv[1]:
-            doList = True
-        else:
-            print("No valid case is given.\n-I for ignoring upper/lowercase and -l printing a list of words.")
-    case 4: 
-        inputFile = sys.argv[3]
-        if ("I" in sys.argv[1]) or ("I" in sys.argv[2]):
-            doIgnore = True
-            print("Iam here")
-        if("l" in sys.argv[1]) or ("l" in sys.argv[2]):
-            doList = True
-        else:
-            print("No valid case is given.\n-I for ignoring upper/lowercase and -l printing a list of words.")
-     
+
 #reading the input file
-with open(inputFile, 'r', encoding="utf-8") as reader:
-    inputText = reader.read()
+def getInputText(inputFile):
+    with open(inputFile, 'r', encoding="utf-8") as reader:
+        inputText = reader.read()
+    return inputText
 
-if doIgnore:
-    inputText = inputText.lower()
+def main():
+    doIgnore = False
+    doList = False 
+    match len(sys.argv): 
+        case 1: 
+            print("No input file is given. Please enter a file name after the scriptname and retry")
+            exit()
+        case 2: 
+            inputText = getInputText(sys.argv[1])
+        case 3: 
+            inputText = getInputText(sys.argv[2])       
+            if "I" in sys.argv[1]:
+                doIgnore = True
+            elif "l" in sys.argv[1]:
+                doList = True
+        case 4: 
+            inputText = getInputText(sys.argv[3])
+            if ("I" in sys.argv[1]) | ("I" in sys.argv[2]):
+                doIgnore = True  
+            if("l" in sys.argv[1]) | ("l" in sys.argv[2]):
+                doList = True            
 
-#splits the inputText into a list of words 
-words = re.findall(r'[^\W_]+',inputText)
+    #converting all upper case to lower case if -I is present in sys argument
+    if doIgnore:
+        inputText = inputText.lower()
 
-wordDict = dict()
-for word in words:
-    if word in wordDict:
-        wordDict[word] += 1
+    #splits the inputText into a list of words 
+    words = re.findall(r'[^\W_]+',inputText)
+
+    #count words
+    wordDict = dict()
+    for word in words:
+        if word in wordDict:
+            wordDict[word] += 1
+        else:
+            wordDict[word] = 1
+
+    #if -l is present in sys argument prints a list otherwise number of words is printed
+    if doList: 
+        sortedKeys = sorted(wordDict.items(),key = lambda x: (-x[1],x[0]),reverse = False)
+        for key in sortedKeys:
+            print(f"{(key[0])}\t{(key[1])}")
     else:
-        wordDict[word] = 1
-
-if doList or doIgnore:    
-    sortedKeys = sorted(wordDict.items(),key = lambda x: (-x[1],x[0]),reverse = False)
-    for key in sortedKeys:
-        print(str(key[0]) + "\t" + str(key[1]))
-else:
-    print(str(len(wordDict)) + "/" + str(len(words)))
-    
+          print(f"{len(wordDict)} / {len(words)}")
+      
+if __name__ == '__main__':
+    main()
