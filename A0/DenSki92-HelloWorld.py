173fdb479c8fa5d93a092b2f982d15d100b6c7ac
DenSki92 <denisskibinski@live.de>
2022-03-07 18:02:05 +0100

    trying out going back to A0

--- A0/DenSki92-HelloWorld.py
+++ A0/DenSki92-HelloWorld.py
@@ -4,9 +4,3 @@ with open(sys.argv[1], "r") as r_file:
     text = r_file.read()
 
 print("Hello World\n" + text)
-
-
-#with open("HelloWorld-test1.out", "w") as w_file:
-#    w_file.write("Hello World\n")
-#    w_file.write(text)
-
