791795d524b64ad6dd37100634d0f9b702966fcd
DenSki92 <denisskibinski@live.de>
2022-03-08 21:23:08 +0100

    Revised Code

--- A1/DenSki92-WordCount.py
+++ A1/DenSki92-WordCount.py
@@ -14,27 +14,22 @@ file_name = args.file_name
 
 with open(file_name, "r") as r_file:
     for line in r_file:
+        if args.ignore:
+            line = line.lower()
         clean_line = re.sub('[^A-Za-z0-9]+', " ", line)
         words = clean_line.split()
         word_count += len(words)
-
-        if args.ignore:
-            for word in words:
-                i_word = word.lower()
-                if i_word not in word_dict:
-                    word_dict[i_word] = 0
-                word_dict[i_word] += 1
-        else:
-            for word in words:
-                if word not in word_dict:
-                    word_dict[word] = 0
-                word_dict[word] += 1
+        for word in words:
+            if word not in word_dict:
+                word_dict[word] = 0
+            word_dict[word] += 1
 
 if args.list:
-    sorted_words_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_words_list = sorted(word_dict.items(), key=lambda x: (-x[1],x[0]))
 
     for word_freq in sorted_words_list:
         print(f"{word_freq[0]}\t{word_freq[1]}")
 
 else:
-    print(f"{len(word_dict)} / {word_count}")
+    print(f"{len(word_dict)}/{word_count}")
+
