4342b3937ec2ee67b6940c088847a0a56bbf3c8e
Carolina Atria <>
2022-03-10 07:47:53 +0100

    A1 now working for both options

--- A1/caroatria-WordCount.py
+++ A1/caroatria-WordCount.py
@@ -3,47 +3,54 @@ import sys
 import re
 import argparse
 
-parser=argparse.ArgumentParser()
-parser.add_argument("-l", help = "create a list")
-parser.add_argument("-I", help = "ignore cases")
-parser.add_argument("filename")
+
 
 def sorting_function (dict):
     sort_by_key = {k:v for (k,v) in sorted(dict.items())}
-    sorted_dict = sorted( sort_by_key.items(), key=lambda item: item[1], reverse=True) 
+    sorted_dict = sorted( sort_by_key.items(), key=lambda x: (-x[1], x[0]))
     return sorted_dict
 
 def ignore_special_char (text):
     global new_file_content
-    new_file_content = re.sub(r"[.?!%^()]", " ", text)
+    new_file_content = re.sub(r"[?!%^()-;,':\"\.]", " ", text)
     return new_file_content
 
 def word_count(option, filename):
     f = open(filename, "r")
     file_content = (f.read())
     new_file_content = ignore_special_char(file_content)
-    if (option == "-l"):
+    if (option == "-l" or option == "-both"):
         counts = dict()
-        word_list= new_file_content.split() 
-        for word in word_list:
-            if word in counts:
-                counts[word] += 1
-            else:
-                counts[word] = 1
-        for k,v in sorting_function(counts):
-            word_frequency = (str(k) + "\t" + str(v))
-            print(word_frequency)
+        if (option == "-both"):
+            word_list= new_file_content.lower().split()
+            for word in word_list:
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word] = 1
+            for k,v in sorting_function(counts):
+                word_frequency = (str(k) + "\t" + str(v))
+                print(word_frequency)
+        else:
+            word_list= new_file_content.split() 
+            for word in word_list:
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word] = 1
+            for k,v in sorting_function(counts):
+                word_frequency = (str(k) + "\t" + str(v))
+                print(word_frequency)
     elif (option ==  "-I"):
         counts = dict()
         word_list= new_file_content.lower().split()
-        print(word_list)
         for word in word_list:
             if word in counts:
                 counts[word] += 1
             else:
                 counts[word] = 1
         number_of_words = len(counts)
-        print(str(len(word_list)) + " / " + str(number_of_words))
+        print(str(number_of_words) + " / " + str(len(word_list)))     
     else:
         counts = dict()
         word_list= new_file_content.split() 
@@ -56,11 +63,17 @@ def word_count(option, filename):
         f.close()
         print(str(number_of_words) + " / " + str(len(word_list)))
 
-def option_function (name_of_script):
-    if (name_of_script == "-l" or name_of_script == "-I"):
-        word_count(sys.argv[1],sys.argv[2])
-    else:
-        word_count("no option",sys.argv[1])
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", nargs="?", type=str)
+parser.add_argument("-l", "--list",  nargs ="?" , help = "create a list")
+parser.add_argument("-I", "--insensitive",  nargs ="?" ,help = "caseinsensitive")
 args=parser.parse_args()
-option_function(sys.argv[0])
-#word_count(args.filename, args.l)
+
+if (len(sys.argv)>3):
+    word_count("-both", sys.argv[3])
+elif args.list:
+    word_count("-l", sys.argv[2])
+elif args.insensitive:
+    word_count("-I", sys.argv[2])
+else:
+    word_count("no option", args.filename)
