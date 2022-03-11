37b1b0d2ada01004d00a45e31c6c86a59971bfb7
gudrun-p <gudrun.poetzelberger@gmail.com>
2022-03-11 15:52:58 +0100

    function that gives out all possible combinations under the cost-limit is finished and functional. All that is missing now is the -o version.

--- A2/gudrun-p-Administration.py
+++ A2/gudrun-p-Administration.py
@@ -15,30 +15,39 @@ def PairCost(a,b, costM, capitals):  # function that returns the cost for assign
     cost = costM[aindex,bindex]
     return cost
 
+def ListCost(L, costM, capitals):  # function that returns the cost for a list of tuples
+    cost = 0
+    for tuple in L:
+        cost = cost + PairCost(tuple[0], tuple[1], costM, capitals)
+    return cost
+
 def administration(paired, rest, costM, costlimit):
     #print("rest = ", rest)
     #print("paired = ", paired)
     #sol = []
     if len(rest)==0:
         SolOutput(paired)
-        return paired
+        return
     elif len(rest)==2:
-        paired.extend([rest])  # extend is like append except it does not give a nested list of lists
-        SolOutput(paired)
-        #print("rest added: ", rest)
-        return paired
+        if ListCost(paired, costM, capitals) + PairCost(rest[0], rest[1], costM, capitals) <= costlimit:
+            paired.extend([rest])  # extend is like append except it does not give a nested list of lists
+            SolOutput(paired)
+            #print("rest added: ", rest)
+        return
     else:
         for i in np.arange(1,len(rest)):
-            paired_2 = paired.copy()  # need extended paired for recursive call, but for next for-loop iteration want to undo, therefore copy
-            paired_2.extend([[rest[0],rest[i]]])  # add the tuple (0,i) from rest
-            #print("pair extend: ", [[rest[0], rest[i]]])
-            rest_2 = rest[1:i]  # need an extra variable, since x.extend() always returns None
-            rest_2.extend(rest[i+1:])  # rest without element 0 and without element i
-            #sol.extend([administration(paired_2, rest_2, costM, costlimit)])
-            administration(paired_2, rest_2, costM, costlimit)
-            #print("    sol = ", sol)
+            # only keep working on list with this pair (rest[0], rest[i] IF adding this pair does not make the solution go over the cost limit:
+            if ListCost(paired, costM, capitals) + PairCost(rest[0], rest[i], costM, capitals) <= costlimit:
+                paired_2 = paired.copy()  # need extended paired for recursive call, but for next for-loop iteration want to undo, therefore copy
+                paired_2.extend([[rest[0],rest[i]]])  # add the tuple (0,i) from rest
+                #print("pair extend: ", [[rest[0], rest[i]]])
+                rest_2 = rest[1:i]  # need an extra variable, since x.extend() always returns None
+                rest_2.extend(rest[i+1:])  # rest without element 0 and without element i
+                #sol.extend([administration(paired_2, rest_2, costM, costlimit)])
+                administration(paired_2, rest_2, costM, costlimit)
+                #print("    sol = ", sol)
+        return
         #return sol
-
 
 
 
@@ -46,9 +55,20 @@ def administration(paired, rest, costM, costlimit):
 
 # ---- READING IN THE DATA: ------
 
-f = open(sys.argv[1], "r")  # read in the input
+optim = False  # True if -o is given as argument
+
+args = sys.argv
+assert len(args)>=2, "Both the python script and the text-input file must be provided!"
+if len(args)>2:
+    if '-o' in args:
+        optim = True
+    else:
+        print("Warning: Unknown flag was given and is being ignored.")
+
+f = open(sys.argv[-1], "r")  # read in the input (file is the last argument)
 input1 = f.read()
 f.close()
+
 
 input_split=input1.split()  # input split by spaces, get 1D list.
 # We know what format the input will take: First two elements of list are number of capitals and cost limit.
@@ -71,10 +91,14 @@ for i in np.arange(0,nCapitals):
             #get to the correct position in the list. To get the correct column, just add the desired column j.
             #(thinking it through with a smaller matrix can help)
             costM[i,j]=input_split[2+nCapitals+nCapitals*i+j]
+#print("costM =")
 #print(costM)
+
+#List = [['B','K'], ['E', 'G']]
+#print("ListCost(BK, EG) = ", ListCost(List, costM, capitals))
 
 # --- FINISHED READING IN THE DATA -----
 
 #print("PairCost(E,G) = ", PairCost("E","G",costM,capitals))
-capitals=["1","2","3","4","5","6","7","8"]
-print("administration([], capitals, costM, costlimit) = ", administration([], capitals, costM, costlimit))
+#capitals=["1","2","3","4","5","6","7","8"]
+administration([], capitals, costM, costlimit)
