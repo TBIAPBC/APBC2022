856a7c4e7ab02510d0e97ee9b8364193014832c4
AndaLatif <andaramona.latif@gmail.com>
2022-03-17 08:13:18 +0100

    final solution A2

--- A2/AndaLatif-Administration.py
+++ A2/AndaLatif-Administration.py
@@ -13,42 +13,49 @@ def read_from_file(ifn):
         for i in range(len(capitals)):
             if cap != capitals[i]:
                cost[tuple(sorted([cap,capitals[i]]))] = int(line[i]) #since the matrix is symetric
-    f.close()
     return  int(no_of_capitals) , cost_limit, capitals, cost
 
 def Generate_authorities( partial_res, cost_limit, cost, unvisited, opt):
     global optimum
     global res
     if unvisited == []:
+        #print(partial_res)
         if (len(partial_res) == no_of_capitals) and (cost_limit >= optimum):
             if opt == "-o":
+                #print(partial_res)
                 if cost_limit > optimum:
+                    #if not (optimum == cost_limit):
                         optimum = cost_limit
                         res = []
+            #print(optimum, cost_limit)
             
             current = []
-            for i in range(len(partial_res) - 1):
+            for i in range(len(partial_res)-1):
                 if i % 2 == 0:
                    current.append("".join(sorted([partial_res[i], partial_res[i+1]])))
-            current = " ".join(current)       
+            current=" ".join(current)       
             if not(current in res):
                 res.append(current)
             #print(res)
             return  res, partial_res
+    
     if cost_limit < optimum:
+       
        return 
-    if len(partial_res) % 2 == 0:
-        for i in range(len(unvisited)): ##even
-             if i == 0:
-                Generate_authorities( partial_res + [unvisited[i]], cost_limit, cost, unvisited[i+1:], opt)
-             else:
-                 Generate_authorities( partial_res + [unvisited[i]], cost_limit, cost, unvisited[:i] + unvisited[i+1:], opt)
+    
+    if len(partial_res) % 2 == 0: ##even
+                if unvisited==[]:
+                    return
+                Generate_authorities( partial_res + [unvisited[0]], cost_limit, cost, unvisited[1:], opt)
+            
     else:
-         for i in range(len(unvisited)): ##odd
+         for i in range(len(unvisited)): ##even
              if i == 0:
-                Generate_authorities( partial_res + [unvisited[i]], cost_limit-cost[tuple(sorted([partial_res[-1], unvisited[i]]))], cost, unvisited[i+1:], opt)
+                 if cost_limit-cost[tuple(sorted([partial_res[-1], unvisited[i]]))]>=0:
+                    Generate_authorities( partial_res + [unvisited[i]], cost_limit-cost[tuple(sorted([partial_res[-1], unvisited[i]]))], cost, unvisited[i+1:], opt)
              else:
-                Generate_authorities( partial_res + [unvisited[i]], cost_limit-cost[tuple(sorted([partial_res[-1], unvisited[i]]))], cost, unvisited[:i-1] + unvisited[i+1:], opt)
+                 if cost_limit-cost[tuple(sorted([partial_res[-1], unvisited[i]]))]>=0:
+                    Generate_authorities( partial_res + [unvisited[i]], cost_limit-cost[tuple(sorted([partial_res[-1], unvisited[i]]))], cost, unvisited[:i] + unvisited[i+1:], opt)
  
 if __name__ == '__main__':
     arg = sys.argv[0:]   
