2fd7a3e9472e61710b614d2a576f7373bcfa9a0d
Zhasmina Stoyanova <sjasmina1607@gmail.com>
2022-03-20 19:48:14 +0100

    cleaned and finishing touches

--- A2/jas-st-Administration.py
+++ A2/jas-st-Administration.py
@@ -23,57 +23,76 @@ def read_input(file):
             
       return cost_lim, capitals_list, cost_values
 
-def administration(cost, limit, solution, unvisited, interm_cost):
+def administration(cost, limit, solution, to_visit, interm_cost):
      global cost_min
+     global solutions
 
-     ##this mode means that the function is searching for the next available capital for the next authority 
-     if len(unvisited)%2==0 and not len(unvisited)==2:   
-           administration(cost, limit, solution+[unvisited[0]],unvisited[1:], interm_cost)
+     ##this mode means that the function is searching for the available capital to begin the pair for the next authority 
+     if len(to_visit)%2==0 and not len(to_visit)==2:   
+           administration(cost, limit, solution+[to_visit[0]],to_visit[1:], interm_cost)
       
       #end mode when only the last two capitals remain 
-     elif len(unvisited) == 2:
-            if cost.get((unvisited[0],unvisited[1])) <= limit:
-                interm_cost += cost.get((unvisited[0],unvisited[1]))
+     elif len(to_visit) == 2:
+            if interm_cost + cost.get((to_visit[0], to_visit[1])) <= limit:
+                interm_cost += cost.get((to_visit[0],to_visit[1]))
                 if optimum and interm_cost < cost_min:
                       cost_min = interm_cost     
                 elif not optimum:
-                      print("YOU WIN!!!!",solution, interm_cost)
+                      solutions.append(solution + [to_visit[0],to_visit[1]])
 
       #pairing mode where the function finds all capitals that produce sum less than the limit     
      else:
-           for el in unvisited:
-                 if cost.get((solution[-1],el)) <= limit:
-                        new_unvisited = unvisited.copy()
+           for el in to_visit:
+                 if interm_cost + cost.get((solution[-1],el)) <= limit:
+                        new_unvisited = to_visit.copy()
                         new_unvisited.remove(el)
+
+                        ## when the optimum mode is on, it always checks if the current sum doesn't exceed the current lowers optimal sum
                         if optimum and interm_cost+cost.get((solution[-1], el))<=cost_min:
-                              administration(cost, limit - cost.get((solution[-1], el)) , solution+[el], new_unvisited, interm_cost+cost.get((solution[-1],el)))
+                              administration(cost, limit, solution+[el], new_unvisited, interm_cost+cost.get((solution[-1],el)))
                         else:
-                              administration(cost, limit - cost.get((solution[-1], el)) , solution+[el], new_unvisited, interm_cost+cost.get((solution[-1],el))) 
+                              administration(cost, limit, solution+[el], new_unvisited, interm_cost+cost.get((solution[-1],el))) 
+
+def input_verifier():
+      if len(sys.argv)<2 or len(sys.argv)>3:
+            print("Please provide the correct arguments! \n  file.py -o(optional) input_file.%txt")
+      elif len(sys.argv)==3 and "-o" not in sys.argv[1]:
+            print("Please provide the correct arguments! \n  file.py -o(optional) input_file.%txt")
+      else:
+            return True
+      
 
 
 if __name__ == "__main__":
 
-##reading the file and assigning variables
-      input_file = read_input(sys.argv[-1]) 
+      if input_verifier():
 
-      cost_values = input_file[2]
-      capitals_list = input_file[1]
-      cost_lim = input_file[0] 
+            ##reading the file and assigning variables
+            input_file = read_input(sys.argv[-1]) 
 
-      if "-o" in sys.argv:
-            optimum = True
-      else:
-            optimum = False
+            cost_values = input_file[2]
+            capitals_list = input_file[1]
+            cost_lim = input_file[0] 
 
-      begin = []  
-      intermediate_cost = 0     
-      cost_min = input_file[0]  
+            if "-o" in sys.argv:
+                  optimum = True
+            else:
+                  optimum = False
 
-      if optimum:
-            administration(cost_values, cost_lim, begin, capitals_list, intermediate_cost)
-            print(cost_min)
-      else:
-            print(administration(cost_values, cost_lim, begin, capitals_list, intermediate_cost))
+            begin = []  
+            intermediate_cost = 0     
+            cost_min = input_file[0] 
+            solutions = [] 
+
+            if optimum:
+                  administration(cost_values, cost_lim, begin, capitals_list, intermediate_cost)
+                  print(cost_min)
+            else:
+                  administration(cost_values, cost_lim, begin, capitals_list, intermediate_cost)
+                  
+                  for sol in solutions:
+                        end_list = ["".join(el) for el in zip(sol[::2],sol[1::2])] 
+                        print(*end_list, sep=" ")
 
 
 
