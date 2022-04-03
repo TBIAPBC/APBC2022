450ed8f29ab3fedcf25f4e215ae952507864dbe8
gudrun-p <gudrun.poetzelberger@gmail.com>
2022-04-03 13:35:31 +0200

    can read HV files

--- A3/gudrun-p-Manhattan.py
+++ A3/gudrun-p-Manhattan.py
@@ -1,7 +1,27 @@
 # A3 by Gudrun Pötzelberger, 00275117
 # 1.4.2022
-
 import sys
+import numpy as np
+
+def readHVFile(file):
+    ns = []
+    ew = []
+    firstline = True
+    lines = f.readlines()
+    nsColumns = 0
+    for line in lines:
+        if line[0] != '#':
+            row = line.split()
+            if len(row) != 0:
+                row = [float(i) for i in row]
+                if firstline:
+                    nsColumns = len(row)
+                firstline = False
+                if len(row) < nsColumns:
+                    ew.append(row)
+                else:
+                    ns.append(row)
+    return ns, ew
 
 diagonalFile = False
 printBestPath = False
@@ -9,16 +29,24 @@ printBestPath = False
 args = sys.argv
 #print("args = ", args)
 assert len(args)>=2, "Both the python script and the text-input file must be provided!"
-
 if len(args)>2:
     if '-d' in args:
         diagonalFile = True
     if '-t' in args:
         printBestPath = True
-
 f = open(sys.argv[-1], "r")
-original_text = f.read()
+if not diagonalFile:
+    [ns, ew] = readHVFile(f)
+else:
+    print("diagonal file reading not yet implemented")
 f.close()
 
-def readHVFile(file):
-    print("to be implemented")
+"""
+print("ns = ", ns)
+print("ew = ", ew)
+print("ns[1][1] = ", ns[1][2])
+print("ew[1][1] = ", ew[2][1])
+print("npparray(ns) = ", np.array(ns))
+"""
+
+
