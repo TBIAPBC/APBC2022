d99fd090935c983d2cc351f91b6a7262fdbcf820
Lisa Korner <lisa_korner@hotmail.de>
2022-04-05 18:43:36 +0200

    removed numpy.any(diagonals) and made findPath recursive

--- A3/renrok-Manhattan.py
+++ A3/renrok-Manhattan.py
@@ -3,6 +3,9 @@ import sys
 
 import numpy
 
+# need to be increased for the second diagonal input file.
+sys.setrecursionlimit(2000)
+
 
 def readInputFile(inputFile):
     with open(inputFile, 'r', encoding="utf-8") as reader:
@@ -54,43 +57,39 @@ def calculateMatrix(northSouth, westEast, diagonals, doDiagonal):
         for j in range(1, D.shape[1]):
             southStep = D[i - 1, j] + northSouth[i - 1, j]  # one step south
             eastStep = D[i, j - 1] + westEast[i, j - 1]  # one step east
-            if doDiagonal & numpy.any(diagonals):
+            if doDiagonal:
                 diagonalStep = D[i - 1, j - 1] + diagonals[i - 1, j - 1]  # one diagonal (left-right) step
                 D[i, j] = max(diagonalStep, southStep, eastStep)
             else:
                 D[i, j] = max(southStep, eastStep)
 
         currentValue = D[i, j]
-    return D, currentValue
+    return D, round(currentValue, 2)
 
 
-def findPath(D, northSouth, westEast, diagonals, doDiagonal):
-    i = D.shape[0] - 1
-    j = D.shape[1] - 1
-    path = ''
-    while True:
-        southStep = D[i - 1, j] + northSouth[i - 1, j]
-        eastStep = D[i, j - 1] + westEast[i, j - 1]
-        if doDiagonal & diagonals.any():
-            diagonalStep = D[i - 1, j - 1] + diagonals[i - 1, j - 1]
-        else:
-            diagonalStep = math.inf
+def findPath(i, j, D, northSouth, westEast, diagonals, doDiagonal):
+    # terminates when the position D_0_0 is reached
+    if ((i == 0) & (j == 0)) | (i < 0) | (j < 0):
+        return ''
 
-        # traceback the steps that have been made to receive the path
-        if D[i, j] == southStep:
-            path = 'S' + path
-            i -= 1  # one step north
-        elif D[i, j] == eastStep:
-            path = 'E' + path
-            j -= 1  # one step west
-        elif D[i, j] == diagonalStep:
-            path = 'D' + path
-            i -= 1  # one diagonal (right-left) step
-            j -= 1
+    southStep = D[i - 1, j] + northSouth[i - 1, j]
+    eastStep = D[i, j - 1] + westEast[i, j - 1]
+    if doDiagonal:
+        diagonalStep = D[i - 1, j - 1] + diagonals[i - 1, j - 1]
+    else:
+        diagonalStep = math.inf
 
-        # terminates when the position D_0_0 is reached
-        if (i == 0) & (j == 0):
-            return path
+    # traceback the steps that have been made to receive the path
+    if D[i, j] == southStep:
+        i -= 1  # one step north
+        return findPath(i, j, D, northSouth, westEast, diagonals, doDiagonal) + 'S'
+    elif D[i, j] == eastStep:
+        j -= 1  # one step west
+        return findPath(i, j, D, northSouth, westEast, diagonals, doDiagonal) + 'E'
+    elif D[i, j] == diagonalStep:
+        i -= 1  # one diagonal (right-left) step
+        j -= 1
+        return findPath(i, j, D, northSouth, westEast, diagonals, doDiagonal) + 'D'
 
 
 def main():
@@ -121,7 +120,7 @@ def main():
     print(bestValue)
 
     if doPrintPath:
-        path = findPath(D, northSouth, westEast, diagonals, doDiagonal)
+        path = findPath(D.shape[0] - 1, D.shape[1] - 1, D, northSouth, westEast, diagonals, doDiagonal)
         print(path)
 
 
