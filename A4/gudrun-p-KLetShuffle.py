edb1796814c9330f3fd49ba6caf1df41640fd633
gudrun-p <gudrun.poetzelberger@gmail.com>
2022-04-10 22:24:55 +0200

    can create 2D dictionary-adjacency-matrix

--- A4/gudrun-p-KLetShuffle.py
+++ A4/gudrun-p-KLetShuffle.py
@@ -23,5 +23,32 @@ f.close()
 if not sequence[-1].isalnum():  # remove the \n that is often at the end of the input files
     sequence = sequence[0:-1]
 
-print("N = ", N)
-print("k = ", k)
+sequence = "CUUUUGCUAG"  # for testing
+k = 3  # for testing
+#"""
+adjacencyDictionary = {}  # I want to create a 2D dictionary "matrix", aka a dictionary of dictionaries,
+## so that I can call things like adjacentDictionary['UU']['UC'], and the result is the number of edges 'UU'->'UC'
+## I am using a dictionary and not an array or list because I need to be able to call it by key (subsequence-string, e.g. 'UC')
+i = 0
+while i < len(sequence)-(k-2):
+    print(sequence[i:i+k-1])
+    if sequence[i:i+(k-1)] not in adjacencyDictionary:
+        adjacencyDictionary[sequence[i:i+(k-1)]] = {}  # add subsequence to dictionary, initialize it as empty dictionary
+    j = i+1  # to make indices clearer: j is just the next (k-1)-let
+    if len(sequence[i:]) > (k-1):  # if another subsequence exists after i
+        if sequence[j:j+(k-1)] not in adjacencyDictionary[sequence[i:i+(k-1)]]:
+            adjacencyDictionary[sequence[i:i+(k-1)]][sequence[j:j+(k-1)]] = 0  # if subsequence j follows subsequence i, then I want to add j to i's sub-dictionary
+        adjacencyDictionary[sequence[i:i + (k - 1)]][sequence[j:j + (k - 1)]] = adjacencyDictionary[sequence[i:i+(k-1)]][sequence[j:j+(k-1)]] + 1
+    i = j
+print(adjacencyDictionary)
+#"""
+"""
+testDict = {"CU": {"CU": 0, "UU": 1, "UG": 0},
+            "UU": {"CU": 0, "UU": 2, "UG": 1},
+            "UG": {"CU": 0, "UU": 0, "UG": 0}
+            }
+print(testDict)
+print(testDict["UU"]["UU"])
+teststring = "CUAG"
+print(teststring[0:2] in testDict)
+"""
