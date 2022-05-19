089bc40f87e9e5a9e6ebb70f70556247572ce398
mkohl98 <marcel_kohl2@gmx.de>
2022-05-20 00:01:22 +0200

    initial commit

---
+++ A6/Game/runRobotRace.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import random
+import argparse
+from importlib import import_module
+
+from Game.game_utils import nameFromPlayerId
+from Game.game_utils import Direction as D, MoveStatus
+from Game.game_utils import Tile, TileStatus, TileObject
+from Game.game_utils import Map, Status
+from Game.simulator import Simulator
+from Game.player_base import Player
+
+
+def main(map_, density, viz, fps, number):
+
+    robot_module_names = {"Test":"Game.test-RobotRace",
+                  "Beatme": "Game.beatme-RobotRace"}
+
+    robotmodules = { m:import_module(m) for m in robot_module_names.values() }
+
+    if map_ is not None:
+       m = Map.read(map_)
+    else:
+       m = Map.makeRandom(30, 30, density)
+
+    sim = Simulator(map=m, vizfile=viz, framerate=fps)
+
+    for name, module_name in robot_module_names.items():
+        for p in robotmodules[module_name].players:
+            p.player_modname = name
+            sim.add_player(p)
+
+    sim.play(rounds=number)
